b783319b4ee5e6f748c26d2633e0ff3c5c5e6491
jessamsaquin <jessamsaquin.dev@gmail.com>
2025-04-08 17:25:05 +0800

    Added functions. Computer Choices and User Choices prompt

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("Hello")
+
+
+function play(){
+    console.log("Playyy")
+}
+
+
+function getComputerChoice(){
+    const choices =["Rock", "Paper","Scissors"];
+    const randomIndex = Math.floor(Math.random()* choices.length);
+    return choices [randomIndex]
+
+}  
+
+function getHumanChoice(){
+    let userChoice = prompt("Rock, Paper or Scissors?");
+    userChoice = userChoice.toLowerCase();
+
+    return userChoice;
+}

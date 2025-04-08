0a4577ea26413c039dd23f7ac150423599169632
jessamsaquin <jessamsaquin.dev@gmail.com>
2025-04-08 17:00:37 +0800

    added 2 functions. Game choices and user input

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("Hello")
+
+
+function play(){
+    console.log("Playyy")
+}
+
+
+function getComputerChoice(){
+    const choices =["Rock", "Paper","Scissors"];
+    const randomIndex = Math.floor(Math.random()* choices.length);
+    return choices [randomIndex]
+
+}  
+
+function getHumanChoice(){
+    let userChoice = prompt("Rock, Paper or Scissors?");
+    userChoice = userChoice.toLowerCase();
+
+    return userChoice;
+}
+
+console.log(getHumanChoice())
